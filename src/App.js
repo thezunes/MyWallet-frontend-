b27b498e58dfccb5539d53a4f03cab389c0351bb
Lucas Nunes <120993347+thezunes@users.noreply.github.com>
2023-04-24 21:35:35 -0300

    adjusting deploy

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import HomePage from "./pages/HomePage"
 import SignInPage from "./pages/SignInPage"
 import SignUpPage from "./pages/SignUpPage"
 import TransactionsPage from "./pages/TransactionPage"
-import dotenv from "dotenv"
+// import dotenv from "dotenv"
 
 export default function App() {
 
 
   const apiUrl = process.env.API_URL;
-  dotenv.config()
+  // dotenv.config()
 
 
   return (
